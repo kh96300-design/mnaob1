b0dc2435a552e04056594fdecb9bf6f5e1215654
kh96300-design <kh.96300@gmail.com>
2026-05-25 11:35:46 +0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'jaad-school-v1';
+const ASSETS = [
+  '/',
+  'Index.html',
+  'code.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://fonts.googleapis.com/css2?family=Tajawal:wght@400;500;700&display=swap'
+];
+
+// تثبيت التطبيق وتخزين الملفات
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+// تفعيل التطبيق وجلب الملفات المخزنة عند انقطاع الإنترنت
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
